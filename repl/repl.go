806525a5881cfe094c0ff6b16ba8f6fc2536e70c
Maaz Ullah <mullah@tecniplast.it>
2025-02-04 22:14:18 +0100

    add repl: for now it only loads the tokens

---
+++ repl/repl.go
@@ -0,0 +1,35 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"maz-lang/lexer"
+	"maz-lang/token"
+	"os"
+)
+
+func Run() {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Welcome to the Maz REPL")
+	for {
+		fmt.Print(">> ")
+		input, _ := reader.ReadString('\n')
+
+		l := lexer.New(input)
+		tokens := getTokens(&l)
+		fmt.Printf("%+v\n", tokens)
+	}
+}
+
+func getTokens(l *lexer.Lexer) []token.Token {
+	var res []token.Token
+
+	for {
+		tok := l.NextToken()
+		if tok.Type == token.EOF {
+			return res
+		}
+		res = append(res, tok)
+	}
+}
